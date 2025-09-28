f175cf26ab3ef4b425c07dd5361db9590fd0bf17
Othavio Luis <76593685+OthavioLuis@users.noreply.github.com>
2025-09-28 00:28:04 +0000

    Modificado corpo do site

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Quem é o monstro? Eu ou o tempo?</h1>
+    return (
+        <div>
+            <h1>Quem é o monstro? Eu ou o tempo?</h1>
+            <p>BK</p>
+        </div>
+    )
 }
 
 export default Home;
